ccacf617e9e9c7a7ae71f151246a3fe6219508a0
Luiz Felipe <felipe.pinghang2@gmail.com>
2025-06-29 14:23:44 -0300

    feat: adicionando funcao no IAService de predict

--- src/services/IA.service.ts
+++ src/services/IA.service.ts
@@ -1,7 +1,15 @@
 import axios from "axios";
+import FormData from 'form-data'
 
 export class IAService {
     private readonly baseUrl: string = "http://localhost:8000/"; // Ajustar conforme necessário
+
+    async predict(form: FormData): Promise<{ predict: number }> {
+        const response = await axios.post<{ predict: number }>(this.baseUrl + "predict", form, {
+            headers: form.getHeaders()
+        })
+        return response.data
+    }
 
     async testConnection(): Promise<string> {
         const response = await axios.get<string>(this.baseUrl)
--- test/IAService.test.ts
+++ test/IAService.test.ts
@@ -1,4 +1,6 @@
 import { IAService } from '../src/services/IA.service'
+import FormData from 'form-data'
+import fs from 'fs'
 
 describe('IAService', () => {
     const iaService = new IAService()
@@ -7,4 +9,11 @@ describe('IAService', () => {
         const result = await iaService.testConnection();
         expect(result).toBe('IAService is working!');
     });
+
+    it("Testando predict da imagem", async () => {
+        const form = new FormData()
+        form.append('file', fs.createReadStream('./test/imgs/numero.png'))
+        const result = await iaService.predict(form)
+        expect(result.predict).toBe(7)
+    })
 });
