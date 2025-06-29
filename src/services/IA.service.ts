c073a7def41c3d2712d1c58ecf9e87bc6711cce1
Luiz Felipe <felipe.pinghang2@gmail.com>
2025-06-29 14:46:22 -0300

    docs: adicionando documentacao nas funcoes do IAService

--- src/services/IA.service.ts
+++ src/services/IA.service.ts
@@ -4,6 +4,11 @@ import FormData from 'form-data'
 export class IAService {
     private readonly baseUrl: string = "http://localhost:8000/"; // Ajustar conforme necessário
 
+    /**
+     * Conexão com a IA, analisa uma imagem e retorna o possivel número desenhado nela.
+     * @param form Imagem a ser analizada.
+     * @returns O número mais provavel a ser da imagem.
+     */
     async predict(form: FormData): Promise<{ predict: number }> {
         const response = await axios.post<{ predict: number }>(this.baseUrl + "predict", form, {
             headers: form.getHeaders()
@@ -11,6 +16,9 @@ export class IAService {
         return response.data
     }
 
+    /**
+     * @returns Teste de conexão com a IA
+     */
     async testConnection(): Promise<string> {
         const response = await axios.get<string>(this.baseUrl)
         return response.data
